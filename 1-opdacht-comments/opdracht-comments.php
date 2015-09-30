faa1b278100619b2b72bb7092c62eb07994dcf01
SeppeDev <goossens.seppe@hotmail.com>
2015-09-30 09:54:52 +0200

    First assignment

---
+++ 1-opdacht-comments/opdracht-comments.php
@@ -0,0 +1,22 @@
+<?php
+
+    /*
+        name    Seppe Goossens
+        e-mail  goossens.seppe@hotmail.com
+    */
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>1 | Opdracht comments</title>
+    </head>
+    <body>
+        
+        <?php echo 'Seppe Goossens' ?>
+
+    </body>
+</html>
